30def8a3457f648b331c0a849facbf696eb8f3bc
wiwium <ketanchaudhari1998@gmail.com>
2021-07-25 17:12:23 +0530

    🔐 added Login Mutation and Resolver

--- server/src/resolvers/UserResolvers.ts
+++ server/src/resolvers/UserResolvers.ts
@@ -1,6 +1,7 @@
 import {Arg, Field, FieldResolver, Mutation, ObjectType, Query, Resolver, Root} from "type-graphql";
 import { User } from "../entity/User";
-import {hash} from "bcryptjs";
+import {compare, hash} from "bcryptjs";
+import {sign} from "jsonwebtoken"
 
 
 
@@ -32,4 +33,36 @@ export class UserResolvers {
     return true;
   }
 
+  
+  // Mutation to login user with boolean return type
+  @Mutation(()=> LoginResponse)
+  async login(
+    @Arg("email", ()=> String) email: string,
+    @Arg("password", ()=> String) password: string) :
+    Promise<LoginResponse> {
+
+    const user = await User.findOne({ where: { email } });
+    if(!user){
+      throw new Error("User not found");
+    }
+
+    const valid = await compare(password, user.password);
+    if(!valid){
+      throw new Error("Invalid password");
+    }
+
+    // login success
+    return {
+      accessToken: sign({userId:user.id}, "my-secret", {
+        expiresIn: "15m",
+      }),
+    };
+  }
+
+}
+
+@ObjectType()
+class LoginResponse{
+  @Field()
+  accessToken: string;    
 }
