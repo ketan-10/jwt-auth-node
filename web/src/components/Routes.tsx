7b52a4294aa0f22c3a088a89849f376f594cf859
wiwium <ketanchaudhari1998@gmail.com>
2021-08-08 15:29:58 +0530

    📟 refresh Token on page Refresh

--- web/src/components/Routes.tsx
+++ web/src/components/Routes.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Switch, Route, BrowserRouter, Link} from 'react-router-dom';
 import {Home} from '../pages/Home';
 import {Login} from '../pages/Login';
 import {Register} from '../pages/Register';
+import { setAccessToken } from '../TokenStore';
 
 
 export const Routes : React.FC = () => {
+
+  // refreash token on page refresh.
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:4000/refreshTheAccessToken", {
+      credentials: 'include',  // 'uses cookie: containing refresh_token' 
+      method: 'POST',
+    })
+    .then(x => x.json())
+    .then(x=> {
+      console.log(x);
+      setAccessToken(x.accessToken);
+      setLoading(false);
+    })
+  }, [])
+  if(loading) return <div>Loading...</div>
+
   return (
     <BrowserRouter>
       <header>
